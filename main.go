452ce75ea53a21d37c28f3b4a0c44af2627e7de9
K Nishijima <nishijima@r3it.com>
2016-10-29 15:45:13 +0900

    ログ出力を追加

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package hellowebserver
 
 import (
+	"log"
+
 	"fmt"
 	"net/http"
+
+	"github.com/comail/colog"
 )
 
 type HelloWebServer struct{}
@@ -11,12 +15,23 @@ type HelloWebServerConfig struct {
 	Port string
 }
 
+func init() {
+	colog.SetDefaultLevel(colog.LDebug)
+	colog.SetMinLevel(colog.LTrace)
+	colog.SetFormatter(&colog.StdFormatter{
+		Colors: true,
+		Flag:   log.Ldate | log.Ltime | log.Lshortfile,
+	})
+	colog.Register()
+}
+
 func (srv HelloWebServer) handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("debug: handler requset:%v", r.RequestURI)
 	fmt.Fprintf(w, "Hello, World")
 }
 
 func (srv HelloWebServer) Startup(config HelloWebServerConfig) {
-	fmt.Println("HelloWebServer startup port:" + config.Port)
+	log.Printf("info: HelloWebServer startup... port:%v", config.Port)
 	http.HandleFunc("/", srv.handler)
 	http.ListenAndServe(":"+config.Port, nil)
 }
